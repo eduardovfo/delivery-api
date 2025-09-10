d7dc39d5165f5aaa08c29dbe110a58f901597149
eduardovfo <eduardo.vfoliveira@gmail.com>
2025-09-10 02:54:34 -0300

    Ajustes

--- src/main/java/br/com/delivery/application/mapper/OrderMapper.java
+++ src/main/java/br/com/delivery/application/mapper/OrderMapper.java
@@ -3,10 +3,9 @@ package br.com.delivery.application.mapper;
 import br.com.delivery.application.dto.OrderDto;
 import br.com.delivery.domain.entity.Order;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = OrderItemMapper.class)
 public interface OrderMapper {
-    
+
     OrderDto toDto(Order order);
 }
